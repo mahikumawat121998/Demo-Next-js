c411a3848216191857fee976c03edf80d5c611fd
kunal <singhkunal98765@gmail.com>
2022-08-19 12:47:06 +0530

    completed cart page and added a loader bar on top

--- components/Productcard.jsx
+++ components/Productcard.jsx
@@ -1,76 +1,90 @@
-import *  as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions } from '@mui/material';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Button, CardActionArea, CardActions } from "@mui/material";
 import styles from "../styles/Productcard.module.css";
-import MyButton from './myButton';
-import { useRouter } from 'next/router'
-import productContext from '../context/productContext';
+import { useRouter } from "next/router";
+import productContext from "../context/productContext";
 
 const Productcard = (props) => {
-    const router = useRouter()
-    const { product } = props;
-    const { cart, setCart } = React.useContext(productContext)
-    function handleAdd(product) {
-        console.log(product.id)
-        let _cart = { ...cart }
-        if (!_cart.items) {
-            _cart.items = {}
-
-        }
-        if (!_cart.items[product.id]) {
-            _cart.items[product.id] = 1
-        } else {
-            _cart.items[product._id] += 1
-        }
-
-        if (!_cart.totalItems) {
-            _cart.totalItems = 0
-
-        }
-        _cart.totalItems += 1
-        setCart(_cart)
-        console.log(cart)
+  const router = useRouter();
+  const { product } = props;
+  const { cart, setCart } = React.useContext(productContext);
+  function handleAdd(product) {
+    console.log(product.id);
+    let _cart = { ...cart };
+    if (!_cart.items) {
+      _cart.items = {};
     }
-    const handleClick = (id) => {
-        console.log(id)
-        router.push({ pathname: '/productDetails', query: { id: id } })
-
+    if (!_cart.items[product.id]) {
+      _cart.items[product.id] = 1;
+    } else {
+      _cart.items[product._id] += 1;
     }
-    return (
-        <>
 
-            <Card sx={{ maxWidth: 345 }}>
-                <CardActionArea>
-                    <CardMedia
-                        component="img"
-                        height="140"
-                        image={product.image}
-                        alt="product image"
-                    />
-                    <CardContent style={{ padding: '10px' }}>
-                        <Typography gutterBottom variant="h5" component="div">
-                            {product.title.slice(0, 20)}...
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            {product.description.slice(0, 60)}...
-                        </Typography>
-                        <Typography className={styles.cardPrice} gutterBottom variant="h6" component="div">
-                            Price - ₹{product.price}
-                        </Typography>
-                    </CardContent>
-                </CardActionArea>
-                <CardActions>
-                    <Button variant="contained" size="small" color="primary" onClick={() => { handleClick(product.id) }}>More Details</Button>
-                    <Button variant="contained" color="success" size="small" onClick={() => { handleAdd(product) }}>Add to Cart</Button>
-                </CardActions>
-            </Card>
+    if (!_cart.totalItems) {
+      _cart.totalItems = 0;
+    }
+    _cart.totalItems += 1;
+    setCart(_cart);
+  }
+  const handleClick = (id) => {
+    router.push({ pathname: "/productDetails", query: { id: id } });
+  };
+  return (
+    <>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            image={product.image}
+            alt="product image"
+          />
+          <CardContent style={{ padding: "10px" }}>
+            <Typography gutterBottom variant="h5" component="div">
+              {product.title.slice(0, 20)}...
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {product.description.slice(0, 60)}...
+            </Typography>
+            <Typography
+              className={styles.cardPrice}
+              gutterBottom
+              variant="h6"
+              component="div"
+            >
+              Price - ₹{product.price}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button
+            variant="contained"
+            size="small"
+            color="primary"
+            onClick={() => {
+              handleClick(product.id);
+            }}
+          >
+            More Details
+          </Button>
+          <Button
+            variant="contained"
+            color="success"
+            size="small"
+            onClick={() => {
+              handleAdd(product);
+            }}
+          >
+            Add to Cart
+          </Button>
+        </CardActions>
+      </Card>
+    </>
+  );
+};
 
-        </>
-
-    )
-}
-
-export default Productcard            
+export default Productcard;
--- pages/cart.js
+++ pages/cart.js
@@ -1,80 +1,157 @@
-import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import productContext from '../context/productContext';
-import Image from 'next/image'
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import productContext from "../context/productContext";
+import Image from "next/image";
+import Button from "@mui/material/Button";
+import { useRouter } from "next/router";
+import NextNProgress from "nextjs-progressbar";
+
 
 const Cart = () => {
-  const { fetchProducts, products, cart } = React.useContext(productContext)
-  const [orders, setOrders] = React.useState([])
+  const [isloading, setIsloading] = React.useState(false)
+  const { fetchProducts, products, cart, setCart } =
+    React.useContext(productContext);
+  const [orders, setOrders] = React.useState([]);
+  let totalprice = 0;
+  let finalprice;
+  const router = useRouter();
   React.useEffect(() => {
+
     if (cart.items) {
-
-      const { ids } = { ids: Object.keys(cart.items) }
-
-      fetchProducts()
+      const { ids } = { ids: Object.keys(cart.items) };
+      setIsloading(true)
+      fetchProducts();
 
       ids.map((id) => {
-        let [a] = products.filter((product) => { return product.id == id })
-        setOrders((current) => [...current, a])
-      })
+        let [a] = products.filter((product) => {
+          return product.id == id;
+        });
+        setOrders((current) => [...current, a]);
+      });
+      setIsloading(false)
     }
-  }, [])
+  }, []);
+  React.useEffect(() => {
+    setOrders([])
+    if (cart.items) {
+      const { ids } = { ids: Object.keys(cart.items) };
 
+      ids.map((id) => {
+        let [a] = products.filter((product) => {
+          return product.id == id;
+        });
+        setOrders((current) => [...current, a]);
+      });
+    }
+  }, [cart]);
 
+  const handlecheckout = () => {
+    router.push("/products");
+    setCart({ items: "", totalItems: "" });
+  };
 
+  const handleQuantity = (id) => {
+    return cart.items[id];
+  };
 
+  const handlePrice = (id, price) => {
+    finalprice = price * handleQuantity(id);
+    totalprice += finalprice;
+    return finalprice;
+  };
 
+  const handleDelete = (id) => {
+    let _cart = { ...cart }
+    let qt = _cart.items[id]
+    _cart.totalItems -= qt
+    delete _cart.items[id]
+    setCart(_cart)
+  }
 
   return (
-    <>{cart.items ?
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Image</TableCell>
-              <TableCell align="right">Title</TableCell>
-              <TableCell align="right">Quantity</TableCell>
-              <TableCell align="right">Price</TableCell>
-              <TableCell align="right">Action</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {orders.map((order, key) => (
-              <TableRow
-                key={key}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  <Image
+    <>
+      {cart.items ? (
+        <>
+          <NextNProgress
+            color="#29D"
+            startPosition={1.3}
+            stopDelayMs={200}
+            height={4}
+            showOnShallow={true} />
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Image</TableCell>
+                  <TableCell align="right">Title</TableCell>
+                  <TableCell align="right">Quantity</TableCell>
+                  <TableCell align="right">Price</TableCell>
+                  <TableCell align="right">Action</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {orders.map((order, key) => (
+                  <TableRow
+                    key={key}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      <Image
 
-                    src={order.image}
-                    alt="Product image"
-                    width={100}
-                    height={100}
-                  />
+                        src={order.image}
+                        alt="Product image"
+                        width={100}
+                        height={100}
+                      />
 
-
-                </TableCell>
-                <TableCell align="right">{order.title}</TableCell>
-                <TableCell align="right"></TableCell>
-                <TableCell align="right">{order.price}</TableCell>
-                <TableCell align="right"></TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer> : <><div><h2 style={{ margin: '50px' }}>Your Cart is empty</h2></div></>
-    }
-
+                    </TableCell>
+                    <TableCell align="right">{order.title}</TableCell>
+                    <TableCell align="right">
+                      {handleQuantity(order.id)}
+                    </TableCell>
+                    <TableCell align="right">
+                      ₹{handlePrice(order.id, order.price)}
+                    </TableCell>
+                    <TableCell align="right">
+                      <Button onClick={() => { handleDelete(order.id) }} variant="contained" color="error">
+                        Delete
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <div style={{ margin: "3rem" }}>
+            <div>Total Price : ₹{totalprice}</div>
+            <Button
+              style={{ margin: "30px 0px" }}
+              variant="contained"
+              color="primary"
+              size="small"
+              onClick={() => {
+                handlecheckout();
+              }}
+            >
+              CHECK OUT
+            </Button>
+          </div>
+        </>
+      ) : (
+        <>
+          <div>
+            <h2 style={{ margin: "50px" }}>Your Cart is empty </h2>
+          </div>
+        </>
+      )}
     </>
+  );
+};
 
-  )
-}
-
-export default Cart
+export default Cart;
